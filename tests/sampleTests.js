b6751ddd340245927b7770596bb3f1b894c939f3
Ivan Budiselic <ivan.budiselic@gmail.com>
2011-09-16 00:24:25 +0800

    added assertValueArrayEquals and Not versions for values and arrays, fixed sampleTests.js

--- tests/sampleTests.js
+++ tests/sampleTests.js
@@ -1,13 +1,27 @@
 var T = require("./node-test-framework.js");
+
 var tests = [
-    new T.Test("sampleTest", function() {
-        T.assertEquals("abc", "xyz");
+    new T.Test("different strings", function() {
+        T.assertNotEquals("abc", "xyz");
     }),
-    new T.Test("sampleTest2", function() {
-        T.assertEquals(0, "xyz");
-    }),
-    new T.Test("sampleTest3", function() {
+    new T.Test("equal strings", function() {
         T.assertEquals("xyz", "xyz");
     }),
+    new T.Test("number !== string", function() {
+        T.assertNotEquals(0, "xyz");
+    }),
+    new T.Test("different length arrays", function() {
+        T.assertValueArrayNotEquals([1,2], [1,2,3]);
+    }),
+    new T.Test("same length different arrays", function() {
+        T.assertValueArrayNotEquals([1,2,4], [1,2,3]);
+    }),
+    new T.Test("equal int arrays", function() {
+        T.assertValueArrayEquals([1,2,3], [1,2,3]);
+    }),
+    new T.Test("equal mixed type arrays", function() {
+        T.assertValueArrayEquals([1,"2",3], [1,"2",3]);
+    }),
 ];
-T.runTests(tests, function() {console.log("setup");}, function() {console.log("teardown");});
+
+T.runTests(tests);
